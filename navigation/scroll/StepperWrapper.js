5558d4d187bd7fc09a01a6203fcfceafcd41a6da
facobackup <gustavomicaelbarbosa@gmail.com>
2021-11-17 21:23:03 -0300

    ScrollStepper

---
+++ navigation/scroll/StepperWrapper.js
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+
+export default function StepperWrapper(props){
+    return props.children ? props.children : <></>
+}
+StepperWrapper.propTypes={
+    className: PropTypes.string,
+    styles: PropTypes.object,
+    children: PropTypes.node
+}
